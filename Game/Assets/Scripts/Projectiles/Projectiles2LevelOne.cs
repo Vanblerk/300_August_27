a3ac127944e145d28828d1fe237b3eee69b7ea0c
ClumsyNinjaXD <ninatji@gmail.com>
2018-09-02 20:12:14 +0200

    Added my changes to level1 and tutorial
    projectiles working

---
+++ Game/Assets/Scripts/Projectiles/Projectiles2LevelOne.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles2LevelOne : MonoBehaviour {
+
+	GameObject poison;
+	//GameObject poison2;
+	GameObject ground;
+	GameObject ground2;
+	GameObject pirate;
+	Vector2 projectileStartingPos;
+	PlayerActionsLevelOne actionScript;
+
+	// Use this for initialization
+	void Start () {
+
+		ground = GameObject.Find ("CaveFloor (9)");
+		poison = GameObject.Find ("Projectiles2");
+		pirate = GameObject.Find ("Character");
+		projectileStartingPos.x = poison.transform.position.x;
+		projectileStartingPos.y = poison.transform.position.y;
+		actionScript = (PlayerActionsLevelOne) pirate.GetComponent(typeof(PlayerActionsLevelOne));
+
+		//poison.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+		if (coll.gameObject == ground) {
+			//poison.SetActive (false);
+			poison.transform.position = projectileStartingPos;
+			poison.SetActive (true);
+			actionScript.ThrowProjectile2(true);
+
+
+		}
+
+		if (coll.gameObject == pirate) {
+			//poison.SetActive (false);
+			poison.transform.position = projectileStartingPos;
+			poison.SetActive (true);
+			actionScript.ThrowProjectile2(true);
+
+
+		}
+	}
+}
