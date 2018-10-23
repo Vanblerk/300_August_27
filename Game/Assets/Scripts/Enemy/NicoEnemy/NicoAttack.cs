3b11ae899baf6f4cc2b82241c1c8c3a8408dc6e6
Vanblerk <vanblerkh@gmail.com>
2018-10-24 01:48:10 +0200

    New boss enemy complete

---
+++ Game/Assets/Scripts/Enemy/NicoEnemy/NicoAttack.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NicoAttack : MonoBehaviour {
+
+	public float timeBetweenAttacks = 50.0f; 	// Time in seconds between attacks
+	public int attackDamage = 1;				// Amount of damage to player
+
+	Animator anim;								// Reference to animator component.
+	GameObject player;							// Reference to the player GameObject.
+	PlayerHealth playerHealth;					// Reference to the player's health.
+	EnemyHealth enemyHealth; 					// Reference to this enemy's health.
+	NicoWalk enemyWalk;						// Reference to the enemy walk script
+	bool playerInRange; 						// Whether player is within the trigger collider and can be damaged
+	float timer;	
+
+	public bool canAttack = true;							// For counting up to next attack				
+
+	void Awake () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent<PlayerHealth> ();
+		enemyHealth = GetComponent<EnemyHealth> ();
+		enemyWalk = GetComponent<NicoWalk> ();
+		anim = GetComponent<Animator>();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+		{			
+			Debug.Log ("TOAJFAS");
+			playerInRange = true;
+			enemyWalk.stopWalking ();
+			anim.SetBool("isAttacking", true);
+			//InvokeRepeating ("Attack", 0.7f, 1.0f);
+		}
+
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+		{			
+			playerInRange = false;
+			anim.SetBool ("isAttacking", false);
+			CancelInvoke ();
+		}
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	}
+
+	void Attack()
+	{
+
+
+		if (playerInRange) {
+
+			playerHealth.PlayerTakeDamage (attackDamage);
+		}
+
+
+	}
+
+
+	IEnumerator AttackTimer()
+	{
+		//canAttack = false;
+		yield return new WaitForSecondsRealtime(1f);
+		//canAttack = true;
+	}
+}
---
+++ Game/Assets/Scripts/Enemy/NicoEnemy/NicoWalk.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NicoWalk : MonoBehaviour {
+	Animator anim;
+	GameObject player;
+
+	public float speed;			//speed of enemy
+	private Transform target;	//Target to follow
+	bool playerInSight;			//To check if player is in line of sight
+
+	void Awake () {
+		playerInSight = false;
+		anim = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject == player)	//if colliding with player
+		{		
+			if (checkPlayerPosition () == "right") {
+				this.transform.eulerAngles = new Vector2(0, 180);
+			} else {
+				this.transform.eulerAngles = new Vector2(0, 0);
+			}
+			playerInSight = true;
+		}
+	}					
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+
+		if (other.gameObject == player)	//if colliding with player
+		{			
+			playerInSight = false;
+		}
+	}
+
+	void Update () {
+		if (playerInSight == true) {
+			WalkToPlayer ();
+		} else {
+			stopWalking ();
+		}
+	}
+
+	string checkPlayerPosition()
+	{
+		float PlayerXPosition = target.position.x;
+		float EnemyXPosition = transform.position.x;
+		if(PlayerXPosition > EnemyXPosition)
+		{
+			return "right";
+		}
+		else{
+			return "left";
+		}
+	}
+
+	public void WalkToPlayer(){
+
+		anim.SetBool ("EnemyIdle", false);
+		anim.SetBool ("isWalking", true);
+		transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+	}
+
+	public void stopWalking(){
+		anim.SetBool ("EnemyIdle", true);
+		anim.SetBool ("isWalking", false);
+	}
+}
